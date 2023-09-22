54301a2f4e55f229510048f1875466dddaa5408d
abdulrehmansafdar <abdulrehmansafdar2928@gmail.com>
2023-09-23 02:58:59 +0500

    content show on tabs

--- resources/views/components/request.blade.php
+++ resources/views/components/request.blade.php
@@ -1,19 +1,32 @@
 <div class="my-2 shadow text-white bg-dark p-1" id="">
-  <div class="d-flex justify-content-between">
-    <table class="ms-1">
-      <td class="align-middle">Name</td>
-      <td class="align-middle"> - </td>
-      <td class="align-middle">Email</td>
-      <td class="align-middle">
-    </table>
-    <div>
-      @if ($mode == 'sent')
-        <button id="cancel_request_btn_" class="btn btn-danger me-1"
-          onclick="">Withdraw Request</button>
-      @else
-        <button id="accept_request_btn_" class="btn btn-primary me-1"
-          onclick="">Accept</button>
-      @endif
-    </div>
-  </div>
+    @if ($mode == 'sent')
+        @foreach ($usersData as $userData)
+            <div class="d-flex justify-content-between">
+                <table class="ms-1">
+                    <td class="align-middle">{{ $userData->name }}</td>
+                    <td class="align-middle"> - </td>
+                    <td class="align-middle">{{$userData->email}}</td>
+                    <td class="align-middle">
+                </table>
+                <div>
+                    <button id="cancel_request_btn_" class="btn btn-danger me-1" onclick="">Withdraw
+                        Request</button>
+                </div>
+            </div>
+        @endforeach
+    @else
+        @foreach ($usersData as $userData)
+            <div class="d-flex justify-content-between">
+                <table class="ms-1">
+                    <td class="align-middle">{{ $userData->name }}</td>
+                    <td class="align-middle"> - </td>
+                    <td class="align-middle">Email</td>
+                    <td class="align-middle">
+                </table>
+                <div>
+                    <button id="accept_request_btn_" class="btn btn-primary me-1" onclick="">Accept</button>
+                </div>
+            </div>
+        @endforeach
+    @endif
 </div>
