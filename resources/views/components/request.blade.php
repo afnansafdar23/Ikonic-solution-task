3b03b03f62d19f9ad04ee0d33bf040ca8df4a1a1
abdulrehmansafdar <abdulrehmansafdar2928@gmail.com>
2023-09-23 19:17:35 +0500

    handle users data

--- resources/views/components/request.blade.php
+++ resources/views/components/request.blade.php
@@ -1,32 +1,34 @@
-<div class="my-2 shadow text-white bg-dark p-1" id="">
-    @if ($mode == 'sent')
-        @foreach ($usersData as $userData)
-            <div class="d-flex justify-content-between">
-                <table class="ms-1">
-                    <td class="align-middle">{{ $userData->name }}</td>
-                    <td class="align-middle"> - </td>
-                    <td class="align-middle">{{$userData->email}}</td>
-                    <td class="align-middle">
-                </table>
-                <div>
-                    <button id="cancel_request_btn_" class="btn btn-danger me-1" onclick="">Withdraw
-                        Request</button>
+<div class="tab-content" id="pills-tabContent">
+    <div class="my-2 shadow text-white bg-dark p-1" id="">
+        @if ($mode == 'sent')
+            @foreach ($usersData as $userData)
+                <div class="d-flex justify-content-between tab-pane fade">
+                    <table class="ms-1">
+                        <td class="align-middle">{{ $userData->name }}</td>
+                        <td class="align-middle"> - </td>
+                        <td class="align-middle">{{ $userData->email }}</td>
+                        <td class="align-middle">
+                    </table>
+                    <div>
+                        <button id="cancel_request_btn_" class="btn btn-danger me-1" onclick="">Withdraw
+                            Request</button>
+                    </div>
                 </div>
-            </div>
-        @endforeach
-    @else
-        @foreach ($usersData as $userData)
-            <div class="d-flex justify-content-between">
-                <table class="ms-1">
-                    <td class="align-middle">{{ $userData->name }}</td>
-                    <td class="align-middle"> - </td>
-                    <td class="align-middle">Email</td>
-                    <td class="align-middle">
-                </table>
-                <div>
-                    <button id="accept_request_btn_" class="btn btn-primary me-1" onclick="">Accept</button>
+            @endforeach
+        @else
+            @foreach ($usersData as $userData)
+                <div class="d-flex justify-content-between tab-pane fade">
+                    <table class="ms-1">
+                        <td class="align-middle">{{ $userData->name }}</td>
+                        <td class="align-middle"> - </td>
+                        <td class="align-middle">{{ $userData->email }}</td>
+                        <td class="align-middle">
+                    </table>
+                    <div>
+                        <button id="accept_request_btn_" class="btn btn-primary me-1" onclick="">Accept</button>
+                    </div>
                 </div>
-            </div>
-        @endforeach
-    @endif
+            @endforeach
+        @endif
+    </div>
 </div>
